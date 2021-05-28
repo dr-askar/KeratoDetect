0bb54df2b9d0a58ffde7bd9b0f35ec27f7505238
dr-askar <49014636+dr-askar@users.noreply.github.com>
2021-05-28 14:39:44 +0300

    Update kc.py

--- kc.py
+++ kc.py
@@ -2,8 +2,8 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image, ImageOps
 import numpy as np
-from tf.keras.models import load_model
-from tf.keras import backend as K
+from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
 from copy import deepcopy
 
 
